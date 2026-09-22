b3d11090ec650ca01d7945c354a0907c589bc435
Ildevdio <diogoswc@gmail.com>
2026-09-22 15:43:05 -0300

    First archives

---
+++ DioPokedex/Services/IPokemonService.cs
@@ -0,0 +1,18 @@
+﻿using DioPokedex.DTOs;
+using DioPokedex.Models;
+using System.Collections.Generic;
+
+namespace DioPokedex.Services
+
+public interface IPokemonService
+{
+    PokemonDTO CreatePkmn(CreatePokemonDTO pokemon);
+    
+    IEnumerable<PokemonDTO> GetPkmnByName(string name);
+    
+    PokemonDTO GetPkmnById(int id);
+    
+    IEnumerable<PokemonDTO> ListPokemons();
+    
+    void DeletePkmn(int id);
+}
