29ba578ae3cff0ef0a1a99c26c47b2632000f2fb
Ildevdio <diogoswc@gmail.com>
2026-09-22 15:59:14 -0300

    Restaura arquivos apagados pelo revert e limpa arquivos de build da ide

---
+++ DioPokedex/Services/PokemonService.cs
@@ -0,0 +1,11 @@
+﻿using DioPokedex.Models;
+using DioPokedex.Services;
+using DioPokedex.DTOs;
+using System.Collections.Concurrent;
+
+namespace DioPokedex.Services;
+
+public class PokemonService : IPokemonService
+{
+    
+}
